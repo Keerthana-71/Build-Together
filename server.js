6443383c52d238ee06a92204fd157e710ea98494
Keerthana <Keerthanasugumaran830@gmail.com>
2026-05-25 16:22:01 +0530

    Connected Railway MySQL

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ const db = mysql.createConnection({
     port     : process.env.DB_PORT     || 31070,
     user     : process.env.DB_USER     || 'root',
     password : process.env.DB_PASSWORD || '',
-    database : process.env.DB_NAME     || 'railway'
+    database : process.env.DB_NAME     || 'buildtogether'
 });
 
 db.connect((err) => {
@@ -67,7 +67,7 @@ db.connect((err) => {
         port     : process.env.DB_PORT     || 31070,
         user     : process.env.DB_USER     || 'root',
         password : process.env.DB_PASSWORD || '',
-        database : process.env.DB_NAME     || 'railway',
+        database : process.env.DB_NAME     || 'buildtogether',
         clearExpired     : true,
         checkExpirationInterval : 900000,
         createDatabaseTable     : true
