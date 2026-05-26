3a7b2593ea27e4c6b369cf9236542c3322f9445d
Keerthana <Keerthanasugumaran830@gmail.com>
2026-05-26 10:12:48 +0530

    final commit

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ db.connect((err) => {
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     port: 587,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
