b81233267b59cc2490deff1b1c99c468389cabbe
Keerthana <Keerthanasugumaran830@gmail.com>
2026-05-26 13:58:43 +0530

    change

--- server.js
+++ server.js
@@ -105,9 +105,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    tls: {
-        rejectUnauthorized: false
-    }
+    family: 4
 });
 transporter.verify((error, success) => {
     if (error) {
